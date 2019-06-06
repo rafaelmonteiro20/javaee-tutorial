dd51b853304deaea00fbf9e10df710cb0a76e40d
Rafael Monteiro <rafajapones@gmail.com>
2019-06-05 21:10:05 -0300

    Simples exemplo usando servlet

---
+++ servlet/humor/src/main/java/com/humor/HumorServlet.java
@@ -0,0 +1,53 @@
+package com.humor;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/report")
+public class HumorServlet extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+		
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Humor</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Humor Servlet em " + request.getContextPath() + "</h1>");
+            String mood = (String) request.getParameter("humor");
+            out.println("<p>Hoje estou: " + mood + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    	response.setContentType("text/html;charset=UTF-8");
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    	processRequest(request, response);
+    }
+	
+}
