ca05302fe06344a5bca6070a8e406a9cc81fb6ee
Rafael Monteiro <rafajapones@gmail.com>
2019-09-12 22:14:49 -0300

    Implementando quiz number

--- jsf/number-quiz/src/main/java/com/quiz/QuizBean.java
+++ jsf/number-quiz/src/main/java/com/quiz/QuizBean.java
@@ -16,6 +16,8 @@ public class QuizBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private int pontos;
+	private int indexAtual;
 	private List<Problema> problemas;
 	
 	public QuizBean() {
@@ -25,6 +27,30 @@ public class QuizBean implements Serializable {
 		problemas.add(new Problema(new int[] { 1, 4, 9, 16, 25 }, 36)); // quadrados
 		problemas.add(new Problema(new int[] { 2, 3, 5, 7, 11 }, 13)); // primos
 		problemas.add(new Problema(new int[] { 1, 2, 4, 8, 16 }, 32)); // potencia de 2
+	}
+	
+	public int getPontos() {
+		return pontos;
+	}
+	
+	public Problema getProblema() {
+		return problemas.get(indexAtual);
+	}
+	
+	public String getResposta() {
+		return "";
+	}
+	
+	public void setResposta(String resposta) {
+		try {
+			int numero = Integer.parseInt(resposta.trim());
+			if(getProblema().getSolucao() == numero) {
+				indexAtual = (indexAtual + 1) % problemas.size();
+				pontos++;
+			}
+		} catch (NumberFormatException e) {
+
+		}
 	}
 	
 	public List<Problema> getProblemas() {
