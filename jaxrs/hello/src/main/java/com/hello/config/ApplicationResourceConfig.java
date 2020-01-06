89208308f393c6503b3bb8ca9016b67f074e9bd7
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2020-01-06 10:47:04 -0300

    Exemplo simples JAX-RS

---
+++ jaxrs/hello/src/main/java/com/hello/config/ApplicationResourceConfig.java
@@ -0,0 +1,9 @@
+package com.hello.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationResourceConfig extends Application {
+
+}
